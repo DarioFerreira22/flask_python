85ea8ca2f3b648da5ec9f55518365dde7c13cfc6
DarioFerreira22 <60893504+DarioFerreira22@users.noreply.github.com>
2025-04-06 17:42:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
+ import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World Build Full Grant !!!</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World INFNET!!!</p></h1>'
 
 @app.route('/api')
 def hello():
